4e7f8c6d852ed8e866feb1dcd03dd199da2b3973
mattedamo <55213300+mattedamo@users.noreply.github.com>
2021-01-29 17:12:54 +0100

    Update delete-overlays-dir.py

--- delete-overlays-dir.py
+++ delete-overlays-dir.py
@@ -2,6 +2,7 @@ import shutil, os
 #you cannot delete prod directory since you cannot delete master branch in code repos
 def main():
       branch = os.environ["CODE_BRANCH"]
+      print(branch)
       tier = os.environ["TIER"]
       list_branch = branch.split("/")
       if(len(list_branch) == 2 and list_branch[0] == "features") or (len(list_branch) == 2 and list_branch[0] == "releases"):
